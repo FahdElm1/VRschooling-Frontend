7448e5d1c8e6d72870cd8480d5e2e63e7b8f1b5b
FahdElm1 <fahdoumnia@gmail.com>
2024-11-21 13:42:15 +0100

    front-end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {LayoutComponent} from "./pages/layout/layout.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {HomeComponent} from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'',
+    component:LayoutComponent,
+    children:[
+      {
+        path:'home',
+        component: HomeComponent,
+        title: 'home'
+      },
+      {
+         path:'profile',
+        component: ProfileComponent,
+        title: 'profile'
+      },
+      {
+        path:'dashboard',
+        component: DashboardComponent,
+        title: 'dashboard'
+      }
+    ]
+  }
+];
